12b28e0d1d27e0e7d3d035d16e9c0af35466703c
elephant97 <elephant0908@gmail.com>
2022-03-06 22:55:29 +0900

    server client 채팅프로그램 기초 스터디

---
+++ src/SimpleClient.java
@@ -0,0 +1,47 @@
+import java.io.BufferedWriter;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class SimpleClient {
+    public static void main(String[] args) {
+        InputStream in_str;
+        OutputStream out_str;
+        String in_msg = null;
+        BufferedReader buf_in;
+        BufferedReader buf_out;
+        BufferedWriter buf_wri = null;
+        PrintWriter pr_wri = null;
+        String out_msg = null;
+
+        try{
+            Socket sock_1 = new Socket("127.0.0.1",5434);
+            in_str = sock_1.getInputStream();
+            out_str = sock_1.getOutputStream();
+            buf_in = new BufferedReader(new InputStreamReader(in_str));
+            buf_out = new BufferedReader(new InputStreamReader(System.in));
+            buf_wri = new BufferedWriter(new OutputStreamWriter(out_str));
+            pr_wri = new PrintWriter(buf_wri,true);
+
+            while(true)
+            {
+                in_msg = buf_in.readLine();
+                System.out.println(in_msg);
+
+                out_msg=buf_out.readLine();
+                if(out_msg.equals("exit"))
+                    break;
+                pr_wri.println("client:"+out_msg);
+            }
+            pr_wri.println("client와의 접속이 종료되었습니다.");
+            pr_wri.close();
+            sock_1.close();
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/SimpleServer.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.net.*;
+
+public class SimpleServer {
+    public static void main(String[] args) {
+        InputStream in_str;
+        BufferedReader buf_in;
+        BufferedReader buf_out;
+        BufferedWriter buf_wri;
+        OutputStream out_str;
+        ServerSocket svr_sock;
+        PrintWriter prin_wri = null;
+        Socket sock_1 = null;
+        String in_msg = null;
+        String out_msg = null;
+
+        try{
+            svr_sock = new ServerSocket(5434);
+            System.out.println("서버 실행 중..");
+            sock_1 = svr_sock.accept();
+            in_str=sock_1.getInputStream();
+            out_str=sock_1.getOutputStream();
+            buf_out=new BufferedReader(new InputStreamReader(System.in));
+            buf_in=new BufferedReader(new InputStreamReader(in_str));
+            buf_wri=new BufferedWriter(new OutputStreamWriter(out_str));
+            prin_wri=new PrintWriter(buf_wri, true);
+            prin_wri.println("server:접속을 환영합니다.");
+
+            while(true){
+                in_msg= buf_in.readLine();
+                System.out.println(in_msg);
+                out_msg= buf_out.readLine();
+                if(out_msg.equals("exit"))
+                    break;
+                prin_wri.println("sever:"+out_msg);
+            }
+            prin_wri.println("sever와의 접속이 종료되었습니다.");
+            prin_wri.close();
+            sock_1.close();
+        }catch(IOException ie){
+            ie.printStackTrace();
+            //prin_wri.close();
+            //sock_1.close();
+        }
+    }
+}
